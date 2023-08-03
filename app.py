30fc20ef55c45bc61861a1dfee3fde59ba3e3af9
Daegyu Han <bigstar77han@gmail.com>
2023-08-03 23:59:16 +0900

    호떡추가

--- app.py
+++ app.py
@@ -57,17 +57,27 @@ def chrstat_post():
 
     URL4 = "https://maple.gg/u/%ED%99%A9%EA%B8%88%EB%AA%A8%EB%9E%98%ED%8F%AD%ED%92%8D"
     headers4 = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data4 = requests.get(URL4,headers=headers3)
+    data4 = requests.get(URL4,headers=headers4)
     soup4 = BeautifulSoup(data4.text, 'html.parser')
 
     image4 = soup4.select_one("#user-profile > section > div.row.row-normal > div.col-lg-4.pt-1.pt-sm-0.pb-1.pb-sm-0.text-center.mt-2.mt-lg-0 > div > div.col-6.col-md-8.col-lg-6 > img").string
     level4 = soup4.select_one("#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.user-summary > ul > li:nth-child(1)").string
     union4 = soup4.select_one("#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > span").string
 
+    URL5 = "https://maple.gg/u/%ED%98%B8%EB%96%A1%EB%A8%B9%EC%9D%84%ED%86%A0%EB%81%BC"
+    headers5 = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data5 = requests.get(URL5,headers=headers5)
+    soup5 = BeautifulSoup(data5.text, 'html.parser')
 
-    level = [level1, level2, level3, level4]
-    union = [union1, union2, union3, union4]
-    image = [image1, image2, image3, image4]
+    image5 = soup5.select_one("#user-profile > section > div.row.row-normal > div.col-lg-4.pt-1.pt-sm-0.pb-1.pb-sm-0.text-center.mt-2.mt-lg-0 > div > div.col-6.col-md-8.col-lg-6 > img").string
+    level5 = soup5.select_one("#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.user-summary > ul > li:nth-child(1)").string
+    union5 = soup5.select_one("#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > span").string
+
+
+
+    level = [level1, level2, level3, level4, level5]
+    union = [union1, union2, union3, union4, union5]
+    image = [image1, image2, image3, image4, image5]
 
     stat = {
         'level' : level,
@@ -182,8 +192,36 @@ def btn_post71():
 
 @app.route("/savebtn71", methods=["GET"])
 def btn_get71():
-    all_btns27 = list(db.blackwizzard71.find({},{'_id':False}))
-    return jsonify({'result': all_btns27})
+    all_btns71 = list(db.blackwizzard71.find({},{'_id':False}))
+    return jsonify({'result': all_btns71})
+
+@app.route("/savebtn63", methods=["POST"])
+def btn_post63():
+    name_receive = request.form['name_give']
+    btn_receive = request.form['btn_give']
+    date_receive = request.form['date_give']
+    onlydate_receive = request.form['onlydate_give']
+
+    doc = {
+        'name':name_receive,
+        'date':date_receive,
+        'onlydate':onlydate_receive,
+        'btn_id':btn_receive
+
+    }
+    db.blackwizzard63.insert_one(doc)
+    
+
+    return jsonify({'msg': '등록 완료!'})
+
+
+@app.route("/savebtn63", methods=["GET"])
+def btn_get63():
+    all_btns63 = list(db.blackwizzard63.find({},{'_id':False}))
+    return jsonify({'result': all_btns63})
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
